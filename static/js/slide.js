ee6f9cb4ff12bfa717cfea59ca99e4e6bef8d466
SinisaJO <73884960+SinisaJO@users.noreply.github.com>
2021-05-26 18:38:12 +0200

    make it better

--- static/js/slide.js
+++
@@ -1,8 +0,0 @@
-images = '.image-container',
-    slide = $(images+' img').innerWidth();
-
-function append(){$(images+' img').first().appendTo($(images));}
-function prepend(){$(images+' img').last().prependTo($(images));}
-
-$('.prev').click(function(){ prepend(); });
-$('.next').click(function(){ append();  });
