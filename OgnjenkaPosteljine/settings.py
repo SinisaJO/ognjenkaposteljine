c106fbe4e9225673cff788cbeeb6ecfa10cbddc5
SinisaJO <73884960+SinisaJO@users.noreply.github.com>
2021-05-29 12:19:38 +0200

    Force SSL redirect

--- OgnjenkaPosteljine/settings.py
+++ OgnjenkaPosteljine/settings.py
@@ -139,3 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+## Forec redirect to https
+
+if os.getcwd() == '/app' :
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False 
